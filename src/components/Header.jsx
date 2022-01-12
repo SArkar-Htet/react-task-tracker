5b8a26d890b0ec6c5d94251fd6f920511de5cee4
Soe Arkar Htet <developer.soearkarhtet@gmail.com>
2022-01-12 20:37:51 +0630

    feat: add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import '../styles/header.css';
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Task Tracker</h1>
+      <button className="btn">Add Task</button>
+    </div>
+  )
+}
+
+export default Header

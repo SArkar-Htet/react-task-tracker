6063b7a287b33ddbb75ab0dd14c382025ff1f59f
Soe Arkar Htet <developer.soearkarhtet@gmail.com>
2022-01-13 11:49:06 +0630

    feat: add dynamic actions

--- src/components/AddTaskForm.jsx
+++ src/components/AddTaskForm.jsx
@@ -1,24 +1,57 @@
+import React, { useState } from 'react';
+
 const AddTaskForm = () => {
+  const [taskName, setTaskName] = useState("");
+  const [dateTime, setDateTime] = useState("");
+  const [reminder, setReminder] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (!taskName) {
+      alert("Please add task");
+    }
+    setTaskName("");
+    setDateTime("");
+    setReminder(false);
   }
 
   return (
     <form action="" className="add-form" onSubmit={handleSubmit}>
       <div className="form-control">
         <label htmlFor="task">Task</label>
-        <input type="text" placeholder="Add Task" id="task" />
+        <input 
+          type="text" 
+          placeholder="Add Task" 
+          id="task" 
+          name='taskName'
+          value={taskName} 
+          onChange={(e) => setTaskName(e.target.value)} 
+        />
       </div>
       <div className="form-control">
         <label htmlFor="date-time">Day & Time</label>
-        <input type="text" placeholder="Add Day & Time" id="date-time" />
+        <input
+          type="text" 
+          placeholder="Add Day & Time" 
+          id="date-time" 
+          name='dateTime'
+          value={dateTime}
+          onChange={(e) => setDateTime(e.target.value)} 
+        />
       </div>
       <div className="form-control form-control-check">
         <label htmlFor="reminder">Set Reminder</label>
-        <input type="checkbox" id="reminder" />
+        <input 
+          type="checkbox" 
+          name='reminder'
+          id="reminder"
+          value={reminder}
+          onChange={(e) => setReminder(e.target.checked)} 
+        />
       </div>
-      <button className="btn btn-block">Save Task</button>
+      <button 
+        className="btn btn-block"
+      >Save Task</button>
     </form>
   )
 }
